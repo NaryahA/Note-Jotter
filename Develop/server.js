228ae4331b306a95254219ad865cd91211ea0ba3
NaryahA <naryah335@gmail.com>
2024-10-28 20:37:28 -0400

    set up express and testing GET routes

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const allNotes = require('./db/db');
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes);
+});
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
