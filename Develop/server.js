6768261c3e18bd81d14be3401422973a444e2aba
NaryahA <naryah335@gmail.com>
2024-11-02 20:25:48 -0400

    added uniqid

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,102 @@
+// Express import
+const express = require("express");
+// File system module import
+const fs = require("fs");
+// Path import
+const path = require("path");
+// Helper method for generating unique ids
+const uniqid = require("uniqid");
+
+// Port
 const PORT = process.env.PORT || 3001;
-const express = require('express');
+
+// Creates new app with express
 const app = express();
-const notes = required('./notes.json')
 
-app.use(express.urlencoded({ extended: true }));
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const allNotes = require('./db/db');
+app.use(express.static("public"));
 
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes);
+// Get route which sends back the index.html page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
+// Get route which sends back the notes.html page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+// Get route -> which reads the db.json file and sends back the parsed JSON data
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    var jsonData = JSON.parse(data);
+    console.log(jsonData);
+    res.json(jsonData);
+  });
 });
-app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+
+// Reads the newly added notes from the request body and then adds them to the db.json file
+const readThenAppendToJson = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeNewNoteToJson(file, parsedData);
+    }
+  });
+};
+
+// Writes data to db.json -> utilized within the readThenAppendToJson function
+const writeNewNoteToJson = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+// Post route -> receives a new note, saves it to request body, adds it to the db.json file, and then returns the new note to the client
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title: title,
+      text: text,
+      id: uniqid(),
+    };
+
+    readThenAppendToJson(newNote, "db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting new note");
+  }
 });
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+// Delete route -> reads the db.json file, uses the json objects uniqids to match the object to be deleted, removes that object from the db.json file, then re-writes the db.json file
+app.delete("/api/notes/:id", (req, res) => {
+  let id = req.params.id;
+  let parsedData;
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      parsedData = JSON.parse(data);
+      const filterData = parsedData.filter((note) => note.id !== id);
+      writeNewNoteToJson("db/db.json", filterData);
+    }
+  });
+  res.send(`Deleted note with ${req.params.id}`);
+});
+
+// App.listen is used to spin up our local server
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
